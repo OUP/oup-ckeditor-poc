01390a6d4950f17de61f9ed1515d6a9f114742ca
ChintanMangukiya-OUP <ChintanMangukiya-OUP@github.com>
2018-08-07 12:16:41 +0530

    GCMS 1114

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ define(function(require, exports, module) {
     require("./fields/oup-ckeditor-field.js");
     
     // import here all extra plugins files
-    //require("./plugins/ouphello/plugin.js");
+    require("./plugins/ouphello/plugin.js");
     //require("./ouphello/icons/ouphello.png");
 
     var Helper = require("./plugins/ouphello/helper.js");
