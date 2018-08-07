ef740125b683469919cb4aa6893694c28fad234d
ChintanMangukiya-OUP <ChintanMangukiya-OUP@github.com>
2018-08-07 19:29:52 +0530

    GCMS 1114

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ define(function(require, exports, module) {
     
     
     // import here all extra plugins JS files
-    require("./plugins/plugin.js");
+    var plugin = require("./plugins/plugin.js");
     
     var action = require("./plugins/action.js");
     // register here using below functionCall
-    helper.registerPlugin("ouphello","ouphello",action.h4Action);
-    helper.registerPlugin("H4","H4",action.h4Action);
+    plugin.registerPlugin("ouphello","ouphello",action.h4Action);
+    plugin.registerPlugin("H4","H4",action.h4Action);
 });
