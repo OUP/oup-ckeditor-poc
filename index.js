020b2275a9ab6d1d8e9e56f78c6f65c759656fc6
ChintanMangukiya-OUP <ChintanMangukiya-OUP@github.com>
2018-08-07 11:31:56 +0530

    GCMS 1114

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 define(function(require) {
     require("./fields/oup-ckeditor-field.js");
 
-    
+    var Helper = require("./ouphello/helper.js");
+    var plugin = require("./ouphello/plugin.js");
     var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
-    var pluginId = "ouphello";
-    var pluginPath = "../../" + moduleId + "/" + "oup-ckeditor-poc" + "/" + pluginId + "/";
-
-            window.CKEDITOR.plugins.addExternal(pluginId, pluginPath, 'plugin.js');
-            // CKEDITOR.config.customConfig = pluginPath + "../ckeditor_config.js";
-
-            if (window.CKEDITOR.config.extraPlugins.length > 0) {
-                window.CKEDITOR.config.extraPlugins += ",";
-            }
-            window.CKEDITOR.config.extraPlugins += pluginId;
-
+    Helper.registerPlugin("ouphello", moduleId);
 });
