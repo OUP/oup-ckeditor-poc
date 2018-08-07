62a9b0efe6950f0fc016dfbab634719a8819a931
ChintanMangukiya-OUP <ChintanMangukiya-OUP@github.com>
2018-08-07 14:36:11 +0530

    GCMS 1114

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ define(function(require, exports, module) {
     
     // import here all extra plugins JS files
     require("./plugins/ouphello/plugin.js");
+    require("./plugins/H4/plugin.js");
     
     var Helper = require("./plugins/helper.js");
     // register here using below functionCall
     Helper.registerPlugin("ouphello");
+    Helper.registerPlugin("H4");
 });
---
+++ plugins/H4/plugin.js
@@ -0,0 +1,44 @@
+define(function(require, exports, module) {
+
+    /**
+     * Supports configuration like this:
+     *
+     * {
+     *     "enableLink": true,
+     *     "enableAltText": true,
+     *     "imagePickerType": "",
+     *     "imagePickerConfig": {},
+     *     "linkPickerType": "",
+     *     "linkPickerConfig": {},
+     *
+     *     "imageClass": "",
+     *     "linkClass": ""
+     * }
+     */
+
+    var CKEDITOR = window.CKEDITOR;
+
+    var pluginName = "H4";
+
+    var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
+
+    CKEDITOR.plugins.add(pluginName, {
+        icons: pluginName,
+        init: function (editor) {
+
+            // button
+            editor.ui.addButton(pluginName, {
+                label: 'H4',
+                icon: '../../../'+moduleId+'/oup-ckeditor-poc/plugins/'+pluginName+'/icons/'+pluginName+'.png',
+                command: pluginName
+            });
+
+            editor.addCommand(pluginName, {
+                exec: function (editor) {
+                    editor.insertHtml("<h4>"+editor.getSelectedHtml().getFirst().getText()+"</h4>");
+                }
+            });
+        }
+    });
+
+});
