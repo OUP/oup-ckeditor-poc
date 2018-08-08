23b2c4255e3b782912d40ba9ae2381271af3a301
ChintanMangukiya-OUP <ChintanMangukiya-OUP@github.com>
2018-08-08 12:55:30 +0530

    GCMS 1095

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 define(function (require, exports, module) {
-    require("./fields/oup-ckeditor-field.js");
 
     // Custom plugins/buttons
     var plugin = require("./plugins/plugin.js");
@@ -8,4 +7,8 @@ define(function (require, exports, module) {
     // register new plugin below. Pass Plugin Name, Plugin Label and Action item as parameters and put action item in action.js using
     plugin.generatePlugin("ouphello", "Hello", action.actionOUPHELLO);
     plugin.generatePlugin("H4", "H4", action.actionH4);
+
+    CKEDITOR.config.extraPlugins += 'bidi';
+
+    require("./fields/oup-ckeditor-field.js");
 });
